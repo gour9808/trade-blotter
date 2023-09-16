d9b077e4141a90542e07ee83a80fcc1cd1466942
gour9808 <gouravsharma614@gmail.com>
2023-09-16 19:17:50 +0530

    commit

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class SidenavComponent {
 
+  constructor(readonly router: Router) {
+
+  }
+  gotoTable() {
+this.router.navigate(['/table'])
+  }
 }
